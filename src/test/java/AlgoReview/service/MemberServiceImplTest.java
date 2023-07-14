112cdcacc6d8530b7ab1b6875e5b181113bfa3ea
Secrethan <rladlfgks422@gmail.com>
2023-07-15 03:16:49 +0900

    init commit

---
+++ src/test/java/AlgoReview/service/MemberServiceImplTest.java
@@ -0,0 +1,20 @@
+package AlgoReview.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceImplTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void jpaSave() {
+    }
+
+    @Test
+    void find() {
+    }
+}
