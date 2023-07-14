8ad2e290da096d1781eda3014ada99b3f6205bf6
Secrethan <rladlfgks422@gmail.com>
2023-07-15 00:37:35 +0900

    커밋

---
+++ src/test/java/algoreview/AlgoreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package algoreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoreviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
