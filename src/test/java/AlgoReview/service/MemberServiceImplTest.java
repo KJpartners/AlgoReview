c26141faaf375fe743cf9984d527df74364a5799
JoungIm Cho <joungim.cho.chloe@gmail.com>
2023-07-15 16:27:57 +0900

    test

--- src/test/java/AlgoReview/service/MemberServiceImplTest.java
+++ src/test/java/AlgoReview/service/MemberServiceImplTest.java
@@ -14,7 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @Transactional
 class MemberServiceImplTest {
     @Autowired
