b69cba3a2f9cec609ec95cb3958b79838e04e35d
JoungIm Cho <joungim.cho.chloe@gmail.com>
2023-07-15 16:49:16 +0900

    test

--- src/test/java/AlgoReview/service/MemberServiceImplTest.java
+++ src/test/java/AlgoReview/service/MemberServiceImplTest.java
@@ -1,20 +1,17 @@
 package AlgoReview.service;
 
+import AlgoReview.AlgoReviewApplication;
 import AlgoReview.domain.Member;
 import AlgoReview.domain.MemberDTO;
 import AlgoReview.repository.MemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
 //@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AlgoReviewApplication.class)
 @Transactional
 class MemberServiceImplTest {
     @Autowired
