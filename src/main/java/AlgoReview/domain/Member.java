659aea7d22ef9b3ddf900d64918c364fb80eeecc
Secrethan <rladlfgks422@gmail.com>
2023-07-15 14:30:45 +0900

    TestCase 수정

--- src/main/java/AlgoReview/domain/Member.java
+++ src/main/java/AlgoReview/domain/Member.java
@@ -1,12 +1,8 @@
 package AlgoReview.domain;
 
 import lombok.*;
-import org.springframework.data.annotation.Id;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 
 @Builder
--- src/test/java/AlgoReview/service/MemberServiceImplTest.java
+++ src/test/java/AlgoReview/service/MemberServiceImplTest.java
@@ -1,20 +1,39 @@
 package AlgoReview.service;
 
+import AlgoReview.domain.Member;
+import AlgoReview.domain.MemberDTO;
+import AlgoReview.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@Transactional
 class MemberServiceImplTest {
-
-    @Test
-    void save() {
-    }
-
+    @Autowired
+    private MemberRepository memberRepository;
     @Test
     void jpaSave() {
+        //given
+        MemberDTO dto = new MemberDTO("kim");
+
+        //when
+        Member member = Member.toEntity(dto);
+        Member SaverdMember = memberRepository.save(member);
+
+        //then
+        Member FindMember = memberRepository.findById(member.getMem_num()).get();
+        Assertions.assertThat(FindMember).isEqualTo(SaverdMember);
+
+
     }
 
-    @Test
-    void find() {
-    }
+
 }
