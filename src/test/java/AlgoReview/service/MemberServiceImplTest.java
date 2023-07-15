1f4b73bd90d11eef0cf569c51499793a7982bcd3
JoungIm Cho <joungim.cho.chloe@gmail.com>
2023-07-15 23:40:54 +0900

    JUnit 4 설정

--- src/test/java/AlgoReview/service/MemberServiceImplTest.java
+++ src/test/java/AlgoReview/service/MemberServiceImplTest.java
@@ -5,18 +5,14 @@ import AlgoReview.domain.Member;
 import AlgoReview.domain.MemberDTO;
 import AlgoReview.repository.MemberRepository;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-//import org.junit.runner.RunWith;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.TestPropertySource;
-//import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-//@RunWith(SpringRunner.class)
-@ExtendWith(SpringExtension.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = AlgoReviewApplication.class)
 @Transactional
 public class MemberServiceImplTest {
