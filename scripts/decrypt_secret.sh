7d6db3bf47c1d43fa8a8fb982cb50efda40f5f89
JoungIm Cho <joungim.cho.chloe@gmail.com>
2023-07-20 05:00:37 +0900

    application.yml 복호화 Test

--- scripts/decrypt_secret.sh
+++ scripts/decrypt_secret.sh
@@ -1,1 +1,3 @@
+cd /path/to/root/directory
+
 gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASSPHRASE" --recipient $GPG_PRIVATE_KEY --output ./src/main/resources/application.yml ./src/main/resources/application.yml.secret
